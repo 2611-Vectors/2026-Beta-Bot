441cfc418a502917aa1c7135824c78c20c908a13
Westley-R <regerwestleyj@my.jccmi.edu>
2026-02-23 17:06:42 -0500

    First Commit 😁

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,100 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotBase;
+
+/**
+ * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
+ * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
+ * (log replay from a file).
+ */
+public final class Constants {
+  public static final Mode simMode = Mode.SIM;
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
+
+    /** Running a physics simulator. */
+    SIM,
+
+    /** Replaying from a log file. */
+    REPLAY
+  }
+
+  public static class ShooterConstants {
+    public static final int LEFT_MOTOR_ID = 0;
+    public static final int RIGHT_MOTOR_ID = 0;
+
+    public static final int LEFT_LINEAR_ACTUATOR_ID = 0;
+    public static final int RIGHT_LINEAR_ACTUATOR_ID = 0;
+  }
+
+  public static class FullSendConstants {
+    public static final int MAIN_MOTOR_ID = 0;
+  }
+
+  public static class IntakeConstants {
+    public static final int PIVOT_MOTOR_ID = 0;
+    public static final int WHEEL_MOTOR_ID = 0;
+
+    public static final int PIVOT_ENCODER_ID = 0;
+  }
+
+  public static class TransitionConstants {
+    public static final int UPPER_LEFT_MOTOR_ID = 0;
+    public static final int UPPER_RIGHT_MOTOR_ID = 0;
+    public static final int LOWER_MOTOR_ID = 0;
+  }
+
+  public static class VisionConstants {
+    // Apriltag Field Layout
+
+    public static AprilTagFieldLayout aprilTagLayout =
+        AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded);
+
+    public static final double FIELD_WIDTH = 16.541;
+    public static final double FIELD_HEIGHT = 8.069;
+
+    // Name of the PhotonVision Reef Camera
+    public static String RightRearCam = "Camera4";
+
+    // Position of the PhotonVision Reef Camera
+    public static Transform3d robotToRightRearCam =
+        new Transform3d(
+            Units.inchesToMeters(0),
+            Units.inchesToMeters(0),
+            Units.inchesToMeters(0),
+            new Rotation3d(0.0, Math.toRadians(0), Math.toRadians(0)));
+
+    // Basic filtering thresholds
+    public static double maxAmbiguity = 0.2;
+    public static double maxZError = 0.2;
+
+    // Standard deviation baselines, for 1 meter distance and 1 tag
+    // (Adjusted automatically based on distance and # of tags)
+    public static double linearStdDevBaseline = 0.02; // Meters
+    public static double angularStdDevBaseline = 0.06; // Radians
+
+    // Standard deviation multipliers for each camera
+    // (Adjust to trust some cameras more than others)
+    public static double[] cameraStdDevFactors = new double[] {1.0};
+
+    // Multipliers to apply for MegaTag 2 observations
+    public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
+    public static double angularStdDevMegatag2Factor =
+        Double.POSITIVE_INFINITY; // No rotation data available
+  }
+}
---
+++ src/main/java/frc/robot/VectorKit/hardware/KrakenX60.java
@@ -0,0 +1,135 @@
+package frc.robot.VectorKit.hardware;
+
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.AngularVelocityUnit;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.VectorKit.tuners.pidTuner;
+import java.util.function.Supplier;
+
+public class KrakenX60 extends TalonFX {
+  private final Slot0Configs slot0Configs = new Slot0Configs();
+  private final MotorOutputConfigs motorOutputConfigs = new MotorOutputConfigs();
+  private final Supplier<TalonFXSimState> m_simState;
+  private final Timer m_simTimer;
+  private double lastTime;
+  private final DCMotorSim m_sim;
+
+  private final VelocityVoltage m_velocityControl = new VelocityVoltage(0).withSlot(0);
+
+  public KrakenX60(int ID) {
+    super(ID);
+
+    m_simTimer = new Timer();
+    m_simState = () -> super.getSimState();
+    m_sim =
+        new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX60Foc(1), 0.001, (18.0 / 70.0)),
+            DCMotor.getKrakenX60Foc(1));
+
+    lastTime = m_simTimer.get();
+  }
+
+  public void updateSim() {
+    m_simState.get().setSupplyVoltage(RobotController.getBatteryVoltage());
+    var motorVoltage = m_simState.get().getMotorVoltageMeasure();
+
+    m_sim.setInputVoltage(motorVoltage.in(Volts));
+    m_sim.update(m_simTimer.get() - lastTime);
+    lastTime = m_simTimer.get();
+
+    m_simState.get().setRawRotorPosition(m_sim.getAngularPosition());
+    m_simState.get().setRotorVelocity(m_sim.getAngularVelocity());
+  }
+
+  public void setVelocity(double vel, AngularVelocityUnit unit) {
+    super.feed();
+    super.setControl(m_velocityControl.withVelocity(RotationsPerSecond.convertFrom(vel, unit)));
+  }
+
+  public void setInverted(InvertedValue direction) {
+    motorOutputConfigs.Inverted = direction;
+    super.getConfigurator().apply(motorOutputConfigs);
+  }
+
+  public void setFollower(KrakenX60 follower, MotorAlignmentValue motorAlignment) {
+    Follower m_followerRequest = new Follower(super.getDeviceID(), motorAlignment);
+    follower.setControl(m_followerRequest);
+  }
+
+  public void setPID(double kP, double kI, double kD) {
+    slot0Configs.kP = kP;
+    slot0Configs.kI = kI;
+    slot0Configs.kD = kD;
+
+    super.getConfigurator().apply(slot0Configs);
+  }
+
+  public void setFF(double kS, double kV) {
+    slot0Configs.kS = kS;
+    slot0Configs.kV = kV;
+
+    super.getConfigurator().apply(slot0Configs);
+  }
+
+  public double getS() {
+    return slot0Configs.kS;
+  }
+
+  public void setS(double kS) {
+    slot0Configs.kS = kS;
+
+    super.getConfigurator().apply(slot0Configs);
+  }
+
+  public double getV() {
+    return slot0Configs.kV;
+  }
+
+  public void setV(double kV) {
+    slot0Configs.kV = kV;
+
+    super.getConfigurator().apply(slot0Configs);
+  }
+
+  public void setP(double kP) {
+    slot0Configs.kP = kP;
+
+    super.getConfigurator().apply(slot0Configs);
+  }
+
+  public void setI(double kI) {
+    slot0Configs.kI = kI;
+
+    super.getConfigurator().apply(slot0Configs);
+  }
+
+  public void setD(double kD) {
+    slot0Configs.kD = kD;
+
+    super.getConfigurator().apply(slot0Configs);
+  }
+
+  public void updateFromTuner(pidTuner tuner) {
+    slot0Configs.kP = tuner.getP();
+    slot0Configs.kI = tuner.getI();
+    slot0Configs.kD = tuner.getD();
+    slot0Configs.kV = tuner.getV();
+    slot0Configs.kS = tuner.getS();
+
+    super.getConfigurator().apply(slot0Configs);
+  }
+}
