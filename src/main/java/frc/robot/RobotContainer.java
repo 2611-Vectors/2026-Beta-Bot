6e2472d10da225e29e70d83cdeceb3c582b6799b
Westley R <regerwestleyj@my.jccmi.edu>
2026-04-09 15:31:12 -0400

    Intake before auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -143,10 +143,7 @@ public class RobotContainer {
         NamedCommands.registerCommand("stopIntake", m_Intake.setIntakeVoltage(() -> 0.0));
         NamedCommands.registerCommand("runTransition", m_Transition.setLowerTransitionRPM(() -> 1000.0));
         NamedCommands.registerCommand("runFullSend", m_FullSend.setFullSendRPM(() -> 5000.0));
-
-        // TODO: DONT RUN INTAKE PIVOT DURING TESTING!!!
-        // NamedCommands.registerCommand("intakeOut", m_Pivot.dumbIntakeOut());
-        NamedCommands.registerCommand("intakeOut", Commands.run(() -> {}));
+        NamedCommands.registerCommand("intakeOut", m_Pivot.dumbIntakeOut());
 
         // Set up auto routines
         autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
@@ -227,7 +224,6 @@ public class RobotContainer {
                 .whileTrue(new ParallelCommandGroup(
                         m_FullSend.manualFullSendRPM(() -> true), m_Transition.manualLowerTransitionRPM(() -> true)));
 
-        // m_DriverController.leftTrigger().toggleOnTrue(m_Shooter.setShooterRPM(() -> 3050.0));
         m_DriverController.leftTrigger().toggleOnTrue(m_Shooter.manualShooterRPM());
 
         m_DriverController
@@ -235,8 +231,7 @@ public class RobotContainer {
                 .toggleOnTrue(
                         new AutoTargetDriverControl(m_Drive, m_Shooter, m_FullSend, m_Transition, m_DriverController));
 
-        // TODO: DONT RUN INTAKE PIVOT DURING TESTING!!!
-        // m_OperatorController.a().whileTrue(m_Pivot.dumbIntakeOut());
+        m_OperatorController.a().whileTrue(m_Pivot.dumbIntakeOut());
     }
 
     /**
